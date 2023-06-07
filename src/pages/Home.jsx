0269f9726fa01a84f4c8f2644343ef0192e67a44
mehdizandian <mahdizandi81@gmail.com>
2023-06-08 01:15:50 +0330

    Navbar with MUI 🚀

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import { Box, Stack } from "@mui/material";
+import Navbar from "../components/Navbar/Navbar";
+
+function Home() {
+  return (
+    <Box>
+      <Navbar />
+      <Stack>
+        <p>Side Bar</p>
+        <p>Feed</p>
+        <p>Right Bar</p>
+        {/* Navbar */}
+        {/* SideBar */}
+        {/* Feed */}
+        {/* RightBar */}
+      </Stack>
+    </Box>
+  );
+}
+
+export default Home;
